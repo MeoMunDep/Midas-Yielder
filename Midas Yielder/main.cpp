1ab082e91f0f9638d9875ff1dee4460f3e4718b1
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-20 00:57:03 +0700

    ?

--- Midas Yielder/main.cpp
+++ Midas Yielder/main.cpp
@@ -1,1 +1,38 @@
-// Only use for pc, sorry guys i don't want to do this but this world has many thieves.
+#include <iostream>
+#include <string>
+
+enum LogLevel {
+    INFO,
+    WARNING,
+    ERROR
+};
+
+void log(LogLevel level, const std::string& message) {
+    std::string colorCode;
+    std::string levelText;
+
+    switch (level) {
+        case INFO:
+            colorCode = "\033[32m";  
+            levelText = "INFO";
+            break;
+        case WARNING:
+            colorCode = "\033[33m";  
+            levelText = "WARNING";
+            break;
+        case ERROR:
+            colorCode = "\033[31m";  
+            levelText = "ERROR";
+            break;
+    }
+
+    std::cout << colorCode << "[" << levelText << "] " << message << "\033[0m" << std::endl;
+}
+
+int main() {
+    log(INFO, "Midas Yielder");
+    log(WARNING, "The best RWA Telegram Miniapp!");
+    log(ERROR, "@Meomundep");
+
+    return 0;
+}
